b56fd683933ac58787f2a16b2e318f74348a3380
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-01-29 15:49:20 +0100

    feat: develop VerifierService

---
+++ ProtoCredentials/OpenID4VC-Prototype/Services/VerifierService.cs
@@ -0,0 +1,14 @@
+﻿using OpenID4VC_Prototype.Models;
+using OpenID4VC_Prototype.Utils;
+
+namespace OpenID4VC_Prototype.Services
+{
+    public class VerifierService
+    {
+        public bool ValidateCredential(VerifiableCredential credential,
+            string issuerPublicKey)
+        {
+            return CryptoUtils.VerifySignature(credential, issuerPublicKey);
+        }
+    }
+}
