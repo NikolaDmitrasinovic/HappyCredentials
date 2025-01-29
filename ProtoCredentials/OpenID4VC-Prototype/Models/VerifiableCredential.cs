989e2b1b7a576f277c2887dbe9ac91ddcc4c7a7f
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-01-29 14:57:45 +0100

    feat: create VC model

---
+++ ProtoCredentials/OpenID4VC-Prototype/Models/VerifiableCredential.cs
@@ -0,0 +1,11 @@
+﻿namespace OpenID4VC_Prototype.Models
+{
+    public class VerifiableCredential
+    {
+        public string IssuerDID { get; set; }
+        public string HolderDID { get; set; }
+        public string CredentialType { get; set; }
+        public Dictionary<string, string> Claims { get; set; }
+        public string Signature { get; set; }
+    }
+}
