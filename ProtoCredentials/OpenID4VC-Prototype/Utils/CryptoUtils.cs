2f7b6ad601d0cbddcd24773492bbbd8f35ba5173
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-01-29 15:45:23 +0100

    feat: develop CryptoUtils

---
+++ ProtoCredentials/OpenID4VC-Prototype/Utils/CryptoUtils.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using OpenID4VC_Prototype.Models;
+
+namespace OpenID4VC_Prototype.Utils
+{
+    public class CryptoUtils
+    {
+        public static string SignData(VerifiableCredential credential, string privateKeyBase64)
+        {
+            var rsa = RSA.Create();
+            rsa.ImportRSAPrivateKey(Convert.FromBase64String(privateKeyBase64), out _);
+
+            var data = $"{credential.IssuerDID}:{credential.HolderDID}:{credential.CredentialType}";
+            var signatureBytes = rsa.SignData
+            (Encoding.UTF8.GetBytes(data), HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+
+            return Convert.ToBase64String(signatureBytes);
+        }
+
+        public static bool VerifySignature(VerifiableCredential credential, string publicKeyBase64)
+        {
+            var rsa = RSA.Create();
+            rsa.ImportRSAPublicKey(Convert.FromBase64String(publicKeyBase64), out _);
+
+            var data = $"{credential.IssuerDID}:{credential.HolderDID}:{credential.CredentialType}";
+            var signatureBytes = Convert.FromBase64String(credential.Signature);
+
+            return rsa.VerifyData(Encoding.UTF8.GetBytes(data), signatureBytes, HashAlgorithmName.SHA256,
+                RSASignaturePadding.Pkcs1);
+        }
+    }
+}
