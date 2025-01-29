eb629d1e1066903b06bc7af35cacc4cdfb3028d1
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-01-29 14:29:43 +0100

    feat: create console proj

---
+++ ProtoCredentials/OpenID4VC-Prototype/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenID4VC_Prototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
