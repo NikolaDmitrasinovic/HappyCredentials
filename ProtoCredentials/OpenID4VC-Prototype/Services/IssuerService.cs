15d63ee3df4c7a9a8421c5f274b38a052b149294
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-01-29 15:45:38 +0100

    feat: develop IssuerService

---
+++ ProtoCredentials/OpenID4VC-Prototype/Services/IssuerService.cs
@@ -0,0 +1,27 @@
+﻿using OpenID4VC_Prototype.Models;
+using OpenID4VC_Prototype.Utils;
+
+namespace OpenID4VC_Prototype.Services
+{
+    public class IssuerService
+    {
+        public VerifiableCredential IssuerCredential
+            (DecentralizedIdentifier issuer, string holderDID)
+        {
+            var credential = new VerifiableCredential
+            {
+                IssuerDID = issuer.DID,
+                HolderDID = holderDID,
+                CredentialType = "Diploma",
+                Claims = new Dictionary<string, string>
+                {
+                    { "University", "PMF" },
+                    { "Curriculum", "Mathematics" }
+                }
+            };
+
+            credential.Signature = CryptoUtils.SignData(credential, issuer.PrivateKey);
+            return credential;
+        }
+    }
+}
