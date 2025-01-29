950f53f78450454cb588136b3af99708cac30c0d
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-01-29 15:04:22 +0100

    feat: develop DID generation (simpified)

---
+++ ProtoCredentials/OpenID4VC-Prototype/Utils/DIDGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using OpenID4VC_Prototype.Models;
+
+namespace OpenID4VC_Prototype.Utils
+{
+    public class DIDGenerator
+    {
+        // Simplified DID generation for presentation purpose
+        public static DecentralizedIdentifier GenerateDID()
+        {
+            var rsa = RSA.Create(2048);
+            string publicKey = Convert.ToBase64String(rsa.ExportRSAPublicKey());
+            string privateKey = Convert.ToBase64String(rsa.ExportRSAPrivateKey());
+
+            return new DecentralizedIdentifier
+            {
+                DID = "did:example:" + Guid.NewGuid(),
+                PublicKey = publicKey,
+                PrivateKey = privateKey
+            };
+        }
+    }
+}
