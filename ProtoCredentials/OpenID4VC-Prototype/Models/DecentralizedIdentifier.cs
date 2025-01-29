108433668e24a768c6035770e24a5be8e6e21bfe
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-01-29 14:55:52 +0100

    feat: create DID model

---
+++ ProtoCredentials/OpenID4VC-Prototype/Models/DecentralizedIdentifier.cs
@@ -0,0 +1,9 @@
+﻿namespace OpenID4VC_Prototype.Models
+{
+    public class DecentralizedIdentifier
+    {
+        public string DID { get; set; }
+        public string PublicKey { get; set; }
+        public string PrivateKey { get; set; }
+    }
+}
