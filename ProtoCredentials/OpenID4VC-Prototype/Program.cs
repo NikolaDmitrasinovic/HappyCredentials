8d800a057b2f61be6126e7ffb360a85343a139e1
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-01-30 12:07:01 +0100

    chore: create prettier presentation

--- ProtoCredentials/OpenID4VC-Prototype/Program.cs
+++ ProtoCredentials/OpenID4VC-Prototype/Program.cs
@@ -16,17 +16,24 @@ namespace OpenID4VC_Prototype
             Console.WriteLine($"Verifier DID: {verifier.DID}");
 
             // Issuing a verifiable credential
+            WriteTitle("Issuing verifiable credential");
             var issuerService = new IssuerService();
-            var credential = issuerService.IssuerCredential
-                (issuer, holder.DID);
+            var credential = issuerService.IssuerCredential(issuer, holder.DID);
 
             Console.WriteLine($"Issuer Credential: {credential.CredentialType} for {credential.HolderDID}");
 
             // Verifier validates the credential
+            WriteTitle("Verifier validates the credential");
             var verifierService = new VerifierService();
             var isValid = verifierService.ValidateCredential(credential, issuer.PublicKey);
 
             Console.WriteLine($"Credential Valid: {isValid}");
+        }
+
+        private static void WriteTitle(string title)
+        {
+            Console.WriteLine();
+            Console.WriteLine("***" + title.ToUpper());
         }
     }
 }
