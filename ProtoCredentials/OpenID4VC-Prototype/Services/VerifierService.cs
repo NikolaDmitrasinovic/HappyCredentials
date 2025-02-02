eb44e104c97e6818a28bd036c4ebf93f40282f06
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-02-02 21:24:11 +0100

    feat: add IsValidDId() to VerifierService

--- ProtoCredentials/OpenID4VC-Prototype/Services/VerifierService.cs
+++ ProtoCredentials/OpenID4VC-Prototype/Services/VerifierService.cs
@@ -1,5 +1,6 @@
 ﻿using OpenID4VC_Prototype.Models;
 using OpenID4VC_Prototype.Utils;
+using System.Text.RegularExpressions;
 
 namespace OpenID4VC_Prototype.Services
 {
@@ -13,11 +14,25 @@ namespace OpenID4VC_Prototype.Services
             if (string.IsNullOrEmpty(issuerPublicKey))
                 return new ValidationResult(false, "Issuer public key is missing");
 
+            if (!IsValidDId(credential.IssuerDId))
+                throw new ArgumentException($"Invalid issuer DID: {credential.IssuerDId}");
+
             var isValid = CryptoUtils.VerifySignature(credential, issuerPublicKey);
 
             return isValid 
                 ? new ValidationResult(true) 
                 : new ValidationResult(false, "Signature verification failed");
+        }
+
+        private static bool IsValidDId(string dId)
+        {
+            if (!string.IsNullOrEmpty(dId))
+            {
+                string patern = @"^did:[a-zA-Z0-9]+:[a-zA-Z0-9\-\.\_]+$";
+                return Regex.IsMatch(dId, patern);
+            }
+
+            return false;
         }
     }
 }
