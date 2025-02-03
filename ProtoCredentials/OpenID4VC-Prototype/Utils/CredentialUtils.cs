c7149750f8b703a9a995ebcf4b819963b0fac880
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-02-03 12:58:04 +0100

    chore: change if condition

--- ProtoCredentials/OpenID4VC-Prototype/Utils/CredentialUtils.cs
+++ ProtoCredentials/OpenID4VC-Prototype/Utils/CredentialUtils.cs
@@ -5,13 +5,13 @@ namespace OpenID4VC_Prototype.Utils
     public static class CredentialUtils
     {
 
-        public static bool IsValidCredential(VerifiableCredential credential)
+        public static bool IsValidCredential(VerifiableCredential? credential)
         {
             if (credential == null) return false;
             if (!DIdUtils.IsValidDId(credential.IssuerDId)) return false;
             if (!DIdUtils.IsValidDId(credential.HolderDId)) return false;
             if (string.IsNullOrEmpty(credential.CredentialType)) return false;
-            if (credential.Claims == null) return false;
+            if (credential.Claims.Count == 0) return false;
             if (string.IsNullOrEmpty(credential.Signature)) return false;
 
             return true;
