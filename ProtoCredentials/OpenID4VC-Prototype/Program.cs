14b4e0535a8638ec3e480159d89e729d4eaa9fa2
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-02-04 23:35:09 +0100

    chore: comment out the log file creation

--- ProtoCredentials/OpenID4VC-Prototype/Program.cs
+++ ProtoCredentials/OpenID4VC-Prototype/Program.cs
@@ -5,7 +5,7 @@ using Serilog;
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
-    .WriteTo.File("../../logs/log.txt", rollingInterval: RollingInterval.Day)
+    //.WriteTo.File("../../logs/log.txt", rollingInterval: RollingInterval.Day)
     .Enrich.WithProperty("Application", "OpenID4VC Prototype")
     .CreateLogger();
 
