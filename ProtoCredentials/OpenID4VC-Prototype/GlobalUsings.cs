460bd486ccc0d58c4bf1e6f2f82feadcf906a01a
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-02-07 00:21:47 +0100

    fix: remove old using directive

--- ProtoCredentials/OpenID4VC-Prototype/GlobalUsings.cs
+++ ProtoCredentials/OpenID4VC-Prototype/GlobalUsings.cs
@@ -1,6 +1,5 @@
 ﻿global using OpenID4VC_Prototype.Configurations;
 global using OpenID4VC_Prototype.Core.Interfaces;
 global using OpenID4VC_Prototype.Core.Models;
-global using OpenID4VC_Prototype.Logging;
 global using OpenID4VC_Prototype.Services;
 global using Serilog;
