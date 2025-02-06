e039a1888e7ddc0c32465f79578dea31d12b4158
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-02-06 13:30:51 +0100

    feat: add IIssuerService

---
+++ ProtoCredentials/OpenID4VC-Prototype/Core/Interfaces/IIssuerService.cs
@@ -0,0 +1,7 @@
+﻿using OpenID4VC_Prototype.Core.Models;
+
+namespace OpenID4VC_Prototype.Core.Interfaces;
+public interface IIssuerService
+{
+    VerifiableCredential IssueCredential(DecentralizedIdentifier issuer, string holderDId);
+}
