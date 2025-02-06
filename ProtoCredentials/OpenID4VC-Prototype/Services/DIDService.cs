bb15f47d3549ef527104a1b2fb7ebce29e416474
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-02-06 11:08:06 +0100

    chore: small improvements

--- ProtoCredentials/OpenID4VC-Prototype/Services/DIDService.cs
+++ ProtoCredentials/OpenID4VC-Prototype/Services/DIDService.cs
@@ -5,8 +5,28 @@ namespace OpenID4VC_Prototype.Services;
 
 public class DIdService
 {
-    // Simplified DId generation for presentation purpose
-    public static DecentralizedIdentifier GenerateDId()
+    public DecentralizedIdentifier GenerateDId(string method = "example")
+    {
+        return method switch
+        {
+            "ion" => GenerateIonDId(),
+            "web" => GenerateWebDId(),
+            "example" => GenerateExampleDId(),
+            _ => throw new ArgumentOutOfRangeException(nameof(method), method, null)
+        };
+    }
+
+    private DecentralizedIdentifier GenerateWebDId()
+    {
+        throw new NotImplementedException();
+    }
+
+    private DecentralizedIdentifier GenerateIonDId()
+    {
+        throw new NotImplementedException();
+    }
+
+    private DecentralizedIdentifier GenerateExampleDId()
     {
         var rsa = RSA.Create(2048);
         var publicKey = Convert.ToBase64String(rsa.ExportRSAPublicKey());
