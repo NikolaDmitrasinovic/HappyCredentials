1827547992e0b874ac74bd5b12dd543bbebb45ba
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-02-06 13:48:03 +0100

    chore: add global using directives

---
+++ ProtoCredentials/OpenID4VC-Prototype/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using OpenID4VC_Prototype.Core.Interfaces;
+global using OpenID4VC_Prototype.Core.Models;
+global using OpenID4VC_Prototype.Logging;
+global using OpenID4VC_Prototype.Services;
+global using Serilog;
