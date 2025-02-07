98837b02ad8d18a824141f086d93e7789e3442a6
Nikola Dmitrasinovic <118458592+NikolaDmitrasinovic@users.noreply.github.com>
2025-02-07 15:11:15 +0100

    Test: Add Unit Tests (#23)
    # Description
    xUnit project is added in order to better cover prototype code.
    
    Only happy path test for `DIdService` is added in this PR.
    
    This will help with further developing existing and adding new functionalities, especially as
    the project becomes more robust.

---
+++ ProtoCredentials/OpenId4VC-Tests/Services/DIdServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using FakeItEasy;
+using Microsoft.Extensions.Options;
+using OpenID4VC_Prototype.Configurations;
+using OpenID4VC_Prototype.Services;
+
+namespace OpenId4VC_Tests.Services;
+public class DIdServiceTests
+{
+    [Fact]
+    public void GenerateDId_ShouldReturnValidDId()
+    {
+        // Arrange
+        var didSettings = new DIdConfiguration()
+        {
+            DefaultKeySize = 2048,
+            DIdPrefix = "did:example:"
+        };
+
+        var fakeOptions = A.Fake<IOptions<DIdConfiguration>>();
+
+        A.CallTo(() => fakeOptions.Value).Returns(didSettings);
+
+        var mockDIdService = new DIdService(fakeOptions);
+
+        // Act
+        var dId = mockDIdService.GenerateDId();
+
+        // Assert
+        Assert.StartsWith("did:example:", dId.DId);
+        Assert.InRange(dId.DId.Length, 48, 48);
+    }
+}
