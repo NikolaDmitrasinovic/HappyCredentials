9292b43fe8008b9340d50059e62567443e9746b4
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-02-08 23:36:47 +0100

    chore: remove global using

--- ProtoCredentials/OpenID4VC-Prototype/GlobalUsings.cs
+++ ProtoCredentials/OpenID4VC-Prototype/GlobalUsings.cs
@@ -1,5 +1,3 @@
-﻿global using OpenID4VC_Prototype.Configurations;
-global using OpenID4VC_Prototype.Core.Interfaces;
-global using OpenID4VC_Prototype.Core.Models;
+﻿global using OpenID4VC_Prototype.Core.Models;
 global using OpenID4VC_Prototype.Services;
 global using Serilog;
