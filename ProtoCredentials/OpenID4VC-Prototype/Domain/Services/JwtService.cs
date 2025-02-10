48c27d17fd06e403dadfae4176125d80118e9ea3
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-02-10 11:55:49 +0100

    feat: add basic token service

---
+++ ProtoCredentials/OpenID4VC-Prototype/Domain/Services/JwtService.cs
@@ -0,0 +1,24 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using OpenID4VC_Prototype.Domain.Models;
+
+namespace OpenID4VC_Prototype.Domain.Services;
+public class JwtService
+{
+    public string CreateJwtVc(VerifiableCredential verifiableCredential)
+    {
+        var claims = new[]
+        {
+            new Claim("issuer", verifiableCredential.IssuerDId),
+            new Claim("holder", verifiableCredential.HolderDId),
+            new Claim("type", verifiableCredential.CredentialType)
+        };
+
+        var token = new JwtSecurityToken(
+            issuer: verifiableCredential.IssuerDId,
+            claims: claims
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
