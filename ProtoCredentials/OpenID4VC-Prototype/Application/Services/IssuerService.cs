1a3e9d061adf5a69913fe9a3713e16fe5eb836ff
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-02-10 13:02:46 +0100

    feat: add IssueJwtVc() to IssuerService

--- ProtoCredentials/OpenID4VC-Prototype/Application/Services/IssuerService.cs
+++ ProtoCredentials/OpenID4VC-Prototype/Application/Services/IssuerService.cs
@@ -8,7 +8,7 @@ using Serilog;
 
 namespace OpenID4VC_Prototype.Application.Services;
 
-public class IssuerService(ICryptoService cryptoService) : IIssuerService
+public class IssuerService(ICryptoService cryptoService, IJwtService jwtService) : IIssuerService
 {
     public VCDto IssueCredential(DIdDto issuer, string holderDId)
     {
@@ -32,5 +32,30 @@ public class IssuerService(ICryptoService cryptoService) : IIssuerService
         credential.Signature = cryptoService.SignData(credential, issuer.PrivateKey);
 
         return credential.Adapt<VCDto>();
+    }
+
+    public string IssueJwtVc(DIdDto issuer, string holderDId)
+    {
+        Log.Information($"Issuing credential for holder DID: {holderDId}");
+
+        if (!DIdValidators.IsValidDId(holderDId))
+            throw new ArgumentException($"Invalid holder DID: {holderDId}");
+
+        var credential = new VerifiableCredential
+        {
+            IssuerDId = issuer.DId,
+            HolderDId = holderDId,
+            CredentialType = "Diploma",
+            Claims = new Dictionary<string, string>
+            {
+                { "University", "PMF" },
+                { "Curriculum", "Mathematics" }
+            }
+        };
+
+        credential.Signature = cryptoService.SignData(credential, issuer.PrivateKey);
+
+        var jwcVc = jwtService.CreateJwtVc(credential);
+        return jwcVc;
     }
 }
