3eba4f76586239c8ba1a973c32b41b081073bad0
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-02-10 21:23:40 +0100

    feat: add presentation method

---
+++ ProtoCredentials/OpenID4VC-Prototype/Presentation/Console/CvFlowPresentation.cs
@@ -0,0 +1,30 @@
+﻿using Mapster;
+using OpenID4VC_Prototype.Application.Interfaces;
+using OpenID4VC_Prototype.Application.Models;
+using OpenID4VC_Prototype.Domain.Models;
+using Serilog;
+
+namespace OpenID4VC_Prototype.Presentation.Console;
+public static class CvFlowPresentation
+{
+    public static void IssueCV(DecentralizedIdentifier issuer, IIssuerService issuerService, DecentralizedIdentifier holder, out VCDto credential)
+    {
+        try
+        {
+            var issuerDto = issuer.Adapt<DIdDto>();
+            credential = issuerService.IssueCredential(issuerDto, holder.DId);
+
+            Log.Information($"Issued Credential: {credential.CredentialType} for {credential.HolderDId}");
+        }
+        catch (ArgumentException ex)
+        {
+            Log.Warning(ex, $"Issuing credential failed: {ex.Message}");
+            credential = new VCDto();
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, $"Unexpected error: {ex.Message}");
+            credential = new VCDto();
+        }
+    }
+}
