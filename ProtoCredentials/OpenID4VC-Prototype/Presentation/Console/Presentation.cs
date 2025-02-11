0f8a855093750d5c5d1a159f81d13b2d4f5fcc5b
Nikola Dmitrasinovic <118458592+NikolaDmitrasinovic@users.noreply.github.com>
2025-02-11 19:08:50 +0100

    Refactor: Extract Presentation from Program class (#30)
    # Description
    The whole creation and verification of an VC is extracted, as is, into a
    new `Presentation` class.
    
    This greatly declutters the `Program` class.

---
+++ ProtoCredentials/OpenID4VC-Prototype/Presentation/Console/Presentation.cs
@@ -0,0 +1,57 @@
+﻿using Mapster;
+using OpenID4VC_Prototype.Application.Interfaces;
+using OpenID4VC_Prototype.Application.Models;
+using OpenID4VC_Prototype.Domain.Models;
+using Serilog;
+
+namespace OpenID4VC_Prototype.Presentation.Console;
+
+public static class Presentation
+{
+    public static void PresentVCFlow(DecentralizedIdentifier issuer, IIssuerService issuerService, DecentralizedIdentifier holder, IVerifierService verifierService)
+    {
+        // Issuing a verifiable credential
+        WriteTitle("Issuing verifiable credential");
+        var credential = new VCDto();
+        try
+        {
+            var issuerDto = issuer.Adapt<DIdDto>();
+            credential = issuerService.IssueCredential(issuerDto, holder.DId);
+
+            System.Console.WriteLine($"Issued Credential: {credential.CredentialType} for {credential.HolderDId}");
+        }
+        catch (ArgumentException ex)
+        {
+            Log.Warning(ex, $"Issuing credential failed: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, $"Unexpected error: {ex.Message}");
+        }
+
+        // Verifier validates the credential
+        WriteTitle("Verifier validates the credential");
+        try
+        {
+            var validationResult = verifierService.ValidateCredential(credential, issuer.PublicKey);
+
+            Log.Information(validationResult.IsValid
+                ? "Credential is valid!"
+                : $"Verification failed: {validationResult.ErrorMessage}");
+        }
+        catch (ArgumentException ex)
+        {
+            Log.Warning(ex, $"Validation error: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, $"Unexpected error: {ex.Message}");
+        }
+    }
+
+    static void WriteTitle(string title)
+    {
+        System.Console.WriteLine();
+        System.Console.WriteLine("***" + title.ToUpper());
+    }
+}
--- ProtoCredentials/OpenID4VC-Prototype/Program.cs
+++ ProtoCredentials/OpenID4VC-Prototype/Program.cs
@@ -1,15 +1,14 @@
-﻿using Mapster;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using OpenID4VC_Prototype.Application.Interfaces;
-using OpenID4VC_Prototype.Application.Models;
 using OpenID4VC_Prototype.Application.Services;
 using OpenID4VC_Prototype.Domain.Interfaces;
 using OpenID4VC_Prototype.Domain.Services;
 using OpenID4VC_Prototype.Infrastructure.Configurations;
+using OpenID4VC_Prototype.Presentation.Console;
 using Serilog;
-using  didConfig = OpenID4VC_Prototype.Infrastructure.Configurations.DIdConfig;
+using didConfig = OpenID4VC_Prototype.Infrastructure.Configurations.DIdConfig;
 
 var builder = Host.CreateDefaultBuilder(args)
     .UseSerilog((hostContext, loggerConfiguration) =>
@@ -45,50 +44,6 @@ Console.WriteLine($"Issuer DID: {issuer.DId}");
 Console.WriteLine($"Holder DID: {holder.DId}");
 Console.WriteLine($"Verifier DID: {verifier.DId}");
 
-// Issuing a verifiable credential
-WriteTitle("Issuing verifiable credential");
-var credential = new VCDto();
-try
-{
-    var issuerDto = issuer.Adapt<DIdDto>();
-    credential = issuerService.IssueCredential(issuerDto, holder.DId);
-
-    Console.WriteLine($"Issued Credential: {credential.CredentialType} for {credential.HolderDId}");
-}
-catch (ArgumentException ex)
-{
-    Log.Warning(ex, $"Issuing credential failed: {ex.Message}");
-}
-catch (Exception ex)
-{
-    Log.Fatal(ex, $"Unexpected error: {ex.Message}");
-}
-
-// Verifier validates the credential
-WriteTitle("Verifier validates the credential");
-try
-{
-    var validationResult = verifierService.ValidateCredential(credential, issuer.PublicKey);
-
-    Log.Information(validationResult.IsValid
-        ? "Credential is valid!"
-        : $"Verification failed: {validationResult.ErrorMessage}");
-}
-catch (ArgumentException ex)
-{
-    Log.Warning(ex, $"Validation error: {ex.Message}");
-}
-catch (Exception ex)
-{
-    Log.Fatal(ex, $"Unexpected error: {ex.Message}");
-}
+Presentation.PresentVCFlow(issuer, issuerService, holder, verifierService);
 
 Log.CloseAndFlush();
-
-return;
-
-static void WriteTitle(string title)
-{
-    Console.WriteLine();
-    Console.WriteLine("***" + title.ToUpper());
-}
