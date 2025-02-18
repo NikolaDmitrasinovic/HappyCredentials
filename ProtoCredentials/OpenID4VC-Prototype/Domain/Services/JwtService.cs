668143c6cd38205532d350b76407be5e34d84537
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-02-18 13:52:30 +0100

    feat: implement interface

--- ProtoCredentials/OpenID4VC-Prototype/Domain/Services/JwtService.cs
+++ ProtoCredentials/OpenID4VC-Prototype/Domain/Services/JwtService.cs
@@ -1,49 +1,45 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
 using System.Security.Cryptography;
-using System.Text;
 using Microsoft.IdentityModel.Tokens;
 using OpenID4VC_Prototype.Domain.Interfaces;
 using OpenID4VC_Prototype.Domain.Models;
 
 namespace OpenID4VC_Prototype.Domain.Services;
-public class JwtService : IJwtService
+public class JwtService(RSA privateKey) : IJwtService
 {
-    public string CreateJwtVc(VerifiableCredential credential, string privateKeyBase64)
+    private readonly RSA _privateKey = privateKey;
+
+    public string CreateJwtVc(VerifiableCredential credential)
     {
-        //var claims = new[]
-        //{
-        //    new Claim("iss", credential.IssuerDId),
-        //    new Claim("sub", credential.HolderDId),
-        //    new Claim("type", credential.CredentialType)
-        //};
-
-        var rsa = RSA.Create();
-        rsa.ImportRSAPrivateKey(Convert.FromBase64String(privateKeyBase64), out _);
-
-        var tokenData = new SecurityTokenDescriptor
+        var tokenDescriptor = new SecurityTokenDescriptor
         {
             Issuer = credential.IssuerDId,
             Claims = new Dictionary<string, object>
             {
                 {"sub", credential.HolderDId },
-                {"credential", credential.Claims }
+                {"credential", credential.Claims },
+                {"iat", DateTimeOffset.UtcNow.ToUnixTimeSeconds()}
             },
-            SigningCredentials = new SigningCredentials(new RsaSecurityKey(rsa), SecurityAlgorithms.RsaSha256)
+            Expires = DateTime.UtcNow.AddMinutes(5),
+            SigningCredentials = new SigningCredentials(new RsaSecurityKey(_privateKey), SecurityAlgorithms.RsaSha256)
         };
 
         var handler = new JwtSecurityTokenHandler();
-        var token = handler.CreateToken(tokenData);
+        var token = handler.CreateToken(tokenDescriptor);
         return handler.WriteToken(token);
     }
 
-    public bool ValidateJwtVc(string jwtVc)
+    public bool ValidateJwtVc(string jwtVc, RSA publicKey)
     {
         var tokenHandler = new JwtSecurityTokenHandler();
+
         var validationParameters = new TokenValidationParameters
         {
             ValidateIssuer = true,
-            ValidateAudience = true,
+            ValidIssuer = "did:example:issuer",
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            IssuerSigningKey = new RsaSecurityKey(publicKey),
             ValidateIssuerSigningKey = true
         };
 
